2c01707db27e62c23a91024ab9cb0cb175d5132c
tapin13 <tapin13@gmail.com>
2022-04-19 03:48:02 +0300

    fix schemas

--- projects/npm-single-lib/src/lib/npm-single-lib.module.ts
+++ projects/npm-single-lib/src/lib/npm-single-lib.module.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { NpmSingleLibComponent } from './npm-single-lib.component';
 
@@ -8,6 +9,7 @@ import { NpmSingleLibComponent } from './npm-single-lib.component';
     NpmSingleLibComponent
   ],
   imports: [
+      CommonModule
   ],
   exports: [
     NpmSingleLibComponent
